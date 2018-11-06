7aa6ad1c9ad0dc579853b721429c463bb2a03a0b
Ezequiel Binker <ezebinker@github.com>
2018-11-06 20:29:09 -0300

    Agrego comentarios

--- src/Spiderman.java
+++ src/Spiderman.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import ia.battle.core.BattleField;
 import ia.battle.core.FieldCell;
 import ia.battle.core.Warrior;
@@ -16,11 +18,26 @@ public class Spiderman extends Warrior{
 
 	@Override
 	public Action playTurn(long tick, int actionNumber) {
-		WarriorData enemyData = BattleField.getInstance().getEnemyData();
-
+		BattleField bf=BattleField.getInstance();
+		WarriorData enemyData = bf.getEnemyData();
+		
+		//bf.calculateDistance para calcular distancia a una celda
+		
+		//ver lo de special items
+		
+		//getEnemyNumber te dice cual es el numero de enemigo actual
+		//A este evento se lo llama 3 veces consecutiva a cada uno.
+		//El tick es el numero del turno, y el actionnumber es 0, 1 y 2. 
+		
 		//TODO: ver que onda el getEnemyNumber. Yo siempre voy a querer que el guerrero se mueva en todos los turnos. 
 		//Son 3 las acciones que se pueden hacer por turno. 1 x cada una, actionNumber indica en cual estas.
 		//Se puede realizar Move, Attack, BuildWall, Suicide o Skip. 
+		
+		//Sobreescribir el método useSpecialItems si no los quiero
+		//Revisar si algun atributo cambió
+		
+		WarriorData hunterData=bf.getHunterData();
+		ArrayList<FieldCell> si=bf.getSpecialItems();
 		
 		if (actionNumber==0) {
 			hasMoved=false;
@@ -48,7 +65,7 @@ public class Spiderman extends Warrior{
 	@Override
 	public void enemyKilled() {
 		System.out.println("Oh Yeah!");
-
+		//Aca poner una variable de, si maté al otro puedo cambiar la estrategia del playturn. 
 	}
 
 	@Override
